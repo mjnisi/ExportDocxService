35b9e5b32ef1d3fc3befd3543cf9f16bf6863069
mjnisi <maria.nisi@edpb.europa.eu>
2022-07-19 16:25:11 +0200

    Comments in a word in a paragraph

--- src/main/java/eu/europa/edpb/services/ExportServiceImpl.java
+++ src/main/java/eu/europa/edpb/services/ExportServiceImpl.java
@@ -1,9 +1,16 @@
 package eu.europa.edpb.services;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.log4j.Logger;
@@ -15,6 +22,7 @@ import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.CommentsPart;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.CTLanguage;
 import org.docx4j.wml.CommentRangeEnd;
 import org.docx4j.wml.CommentRangeStart;
 import org.docx4j.wml.Comments;
@@ -24,6 +32,8 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import com.ximpleware.AutoPilot;
 import com.ximpleware.NavException;
 import com.ximpleware.ParseException;
@@ -48,6 +58,8 @@ public class ExportServiceImpl implements ExportService {
 	
 	final static Logger logger = Logger.getLogger(ExportServiceImpl.class);
 
+	private java.math.BigInteger commentId = BigInteger.valueOf(0);
+				
 
 	@Override
 	public String exportStatement(String xmlDoc) {
@@ -57,26 +69,33 @@ public class ExportServiceImpl implements ExportService {
 		}
 		
 		String EXPORT_TEMPLATE_BVD_FILE = "/templates/StatementEDPB.dotx";
+//		String EXPORT_TEMPLATE_BVD_FILE = "/templates/StatementEDPBcomment3.dotx";
+
+		
+		String jsonPath = "/documents/Proposal_197_2447391798399561462/PROP_ACT_6020660205072156884/media/annot_bill_cl5jc32710002q011gkfo1dto.xml.json";
+		String xmlPath = "/documents/Proposal_197_2447391798399561462/PROP_ACT_6020660205072156884/bill_cl5jc32710002q011gkfo1dto.xml";
 
 		try {
 			
 			mlPackage = createDocxFromTemplateVD(EXPORT_TEMPLATE_BVD_FILE);
 			
-			loadTemplate();
+ 			loadTemplate();
 			setMargins();
-			
-			InputStream isXml = getClass().getResourceAsStream(
-					"/documents/Proposal_197_2447391798399561462/PROP_ACT_6020660205072156884/bill_cl5jc32710002q011gkfo1dto.xml");
+
+			InputStream isXml = getClass().getResourceAsStream(xmlPath);
 			byte[] ba = IOUtils.toByteArray(isXml);
-
-			InputStream isJson = getClass().getResourceAsStream(
-					"/documents/Proposal_197_2447391798399561462/PROP_ACT_6020660205072156884/media/annot_bill_cl5jc32710002q011gkfo1dto.xml.json");
-
+	         
+						
+			List<HashMap<String, String>> annotations = getAnnotationsFromFile(jsonPath);
+	
 			
-			JSONObject obj = new JSONObject(isJson);
-			JSONArray rows = obj.getJSONArray("rows");
-			JSONObject total = obj.getJSONObject("total");
-			JSONArray replies = obj.getJSONArray("replies");
+			// Comments
+			CommentsPart cp = new CommentsPart();
+			mlPackage.getMainDocumentPart().addTargetPart(cp);
+
+			Comments comments = factory.createComments();
+			cp.setJaxbElement(comments);
+
 			
 			VTDGen vg = new VTDGen();
 			vg.setDoc(ba);
@@ -123,14 +142,19 @@ public class ExportServiceImpl implements ExportService {
 			ap.resetXPath();
 			ap.bind(vn);
 			ap.selectXPath("//body/paragraph/content/p/text()");
-
+			//buscar el id de p y buscarlo dentro del array de id comments para ver si hay q crear un commentario.
+			
+			
 			result = -1;
 			mlPackage.getMainDocumentPart().addStyledParagraphOfText(STYLE_CONTENT, "");
 			mlPackage.getMainDocumentPart().addStyledParagraphOfText(STYLE_CONTENT, "");
 
 			while ((result = ap.evalXPath()) != -1) {
-				mlPackage.getMainDocumentPart().addStyledParagraphOfText(STYLE_CONTENT, vn.toString(result));
 				
+				String[] positions = {"10:15"};
+				addCommentToP(vn.toString(result), STYLE_CONTENT , positions);
+//				addCommentToP1(id, vn.toString(result), STYLE_CONTENT , annotations);
+
 				if(logger.isDebugEnabled()){
 					logger.debug("Paragraph : " + vn.toString(result));
 				}
@@ -155,56 +179,7 @@ public class ExportServiceImpl implements ExportService {
 				}
 			}
 
-
-			// Comments
-			CommentsPart cp = new CommentsPart();
-			mlPackage.getMainDocumentPart().addTargetPart(cp);
-
-			Comments comments = factory.createComments();
-			cp.setJaxbElement(comments);
-
-			// Add a comment to the comments part
-			java.math.BigInteger commentId = BigInteger.valueOf(0);
-			Comment theComment = createComment(commentId, "MARIA NISI", null, "my first comment");
-			comments.getComment().add(theComment);
-
-			// Add comment reference to document
-			// P paraToCommentOn =
-			// wordMLPackage.getMainDocumentPart().addParagraphOfText("here is some
-			// content");
-			P p = new P();
-
-			mlPackage.getMainDocumentPart().getContent().add(p);
-
-			// Create object for commentRangeStart
-			CommentRangeStart commentrangestart = factory.createCommentRangeStart();
-			commentrangestart.setId(commentId); // substitute your comment id
-
-			// The actual content, in the middle
-			p.getContent().add(commentrangestart);
-
-			org.docx4j.wml.Text t = factory.createText();
-			t.setValue("hello");
-
-			org.docx4j.wml.R run = factory.createR();
-			run.getContent().add(t);
-
-			p.getContent().add(run);
-
-			// Create object for commentRangeEnd
-			CommentRangeEnd commentrangeend = factory.createCommentRangeEnd();
-			commentrangeend.setId(commentId); // substitute your comment id
-
-			p.getContent().add(commentrangeend);
-
-			p.getContent().add(createRunCommentReference(commentId));
-
-//			System.out.println(mlPackage.getMainDocumentPart().getXML());
-//
-//			// ++, for next comment ...
-//			commentId = commentId.add(java.math.BigInteger.ONE);
-
-			System.out.println(mlPackage.getMainDocumentPart().getContent());
+			System.out.println(mlPackage.getMainDocumentPart().getXML());
 
 			mlPackage.save(new java.io.File("doc2.doc"), Docx4J.FLAG_SAVE_ZIP_FILE);
 
@@ -223,15 +198,57 @@ public class ExportServiceImpl implements ExportService {
 	}
 
 	
+	private List<HashMap<String, String>> getAnnotationsFromFile(String jsonPath) {
+		
+		String json = getResource(jsonPath);
+		logger.debug("json :"+json);
+
+		JSONObject obj = new JSONObject(json);
+		JSONArray rows = obj.getJSONArray("rows");
+		BigInteger total = obj.getBigInteger("total");
+		JSONArray replies = obj.getJSONArray("replies");
+
+		logger.debug("rows :"+rows);
+		logger.debug("total :"+total);
+		logger.debug("replies :"+replies);
+
+		String jsonPathComments = "$.rows[*].target[*].selector[0]";
+		//buscar tb los start y end, hacer un metodo q reciba parrafo, inicio y fin y cree un comentario. Devuelva un id. 
+	
+		DocumentContext jsonContext = JsonPath.parse(json);
+		List<HashMap<String, String>> jsonAnnotations = jsonContext.read(jsonPathComments);
+		logger.debug("jsonComments :" + jsonAnnotations);
+		
+		return jsonAnnotations;
+		
+	}
+
+	
+	
+	private String getResource(String resource) {
+        StringBuilder json = new StringBuilder();
+        try {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream(resource)),
+                            StandardCharsets.UTF_8));
+            String str;
+            while ((str = in.readLine()) != null)
+                json.append(str);
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Caught exception reading resource " + resource, e);
+        }
+        return json.toString();
+    }
+
+	
+	
 	private void setMargins() {
 		mlPackage.getMainDocumentPart().getJaxbElement().getBody().getSectPr().getPgMar().setTop(new BigInteger("0"));
 
 	}
 	
 	
-//	Instanciación del documento:
-//	template de word donde se definen los estilos
-
 	private WordprocessingMLPackage createDocxFromTemplateVD(String template) {
 		WordprocessingMLPackage wordPackage = null;
 		try {
@@ -270,6 +287,90 @@ public class ExportServiceImpl implements ExportService {
 
 	}
 
+	
+	
+	private void addCommentToP(String text, String style, String[] positions ) {
+		
+		P p = mlPackage.getMainDocumentPart().createStyledParagraphOfText(style, "");
+		CTLanguage lang = new CTLanguage();
+		lang.setVal("fr-BE");
+
+		for (String pos : positions) {
+			int startPosition = Integer.parseInt(pos.split(":")[0]);
+			int endPosition = Integer.parseInt(pos.split(":")[1]);
+
+			commentId = commentId.add(java.math.BigInteger.ONE);
+			
+			org.docx4j.wml.R wR = factory.createR();
+			org.docx4j.wml.RPr wRPr = factory.createRPr();
+			org.docx4j.wml.Text wT = factory.createText();
+			
+
+			wT.setValue(text.substring(0, startPosition));
+			wT.setSpace("preserve");
+
+			wRPr.setLang(lang);
+			wR.setRPr(wRPr);
+			wR.getContent().add(wT);
+			p.getContent().add(wR);
+			
+			
+			// Create object for commentRangeStart
+			CommentRangeStart commentrangestart = factory.createCommentRangeStart();
+			commentrangestart.setId(commentId); 
+
+			p.getContent().add(commentrangestart);
+
+			org.docx4j.wml.R wR1 = factory.createR();
+			org.docx4j.wml.RPr wRPr1 = factory.createRPr();
+			org.docx4j.wml.Text wT1 = factory.createText();
+			
+
+			wT1.setValue(text.substring(startPosition, endPosition));
+			wT1.setSpace("preserve");
+			
+			wRPr1.setLang(lang);
+			wR1.setRPr(wRPr1);
+			wR1.getContent().add(wT1);
+
+			p.getContent().add(wR1);
+			
+			
+			// Create object for commentRangeEnd
+			CommentRangeEnd commentrangeend = factory.createCommentRangeEnd();
+			commentrangeend.setId(commentId); // substitute your comment id
+
+			p.getContent().add(commentrangeend);
+
+			org.docx4j.wml.R wR2 = factory.createR();
+			org.docx4j.wml.RPr wRPr2 = factory.createRPr();
+			org.docx4j.wml.Text wT2 = factory.createText();
+			
+			wT2.setValue(text.substring(endPosition));
+			wT2.setSpace("preserve");
+
+			wRPr2.setLang(lang);
+			wR2.setRPr(wRPr2);
+			wR2.getContent().add(wT2);
+			
+			p.getContent().add(wR2);
+
+
+			p.getContent().add(createRunCommentReference(commentId));
+
+			Comment theComment = createComment(commentId, "MARIA NISI", null, "my first comment");
+
+			mlPackage.getMainDocumentPart().getCommentsPart().getJaxbElement().getComment().add(theComment);
+
+		}
+		mlPackage.getMainDocumentPart().getContent().add(p);
+	
+
+	}
+	
+	
+	
+	
 	private org.docx4j.wml.Comments.Comment createComment(java.math.BigInteger commentId, String author, Calendar date,
 			String message) {
 
